da3077d12e1d3a02c351aa0298f395301b844d39
unknown <wendyli8620lw@gmail.com>
2022-11-16 21:23:03 +0100

    modified route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { UserManagementModule } from './user-management/user-management.module';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home'},
   { path: 'home', component: HomeComponent },
-  { path: 'user-management', loadChildren: () => UserManagementModule }
+  { path: 'home/user-management', loadChildren: () => UserManagementModule }
 ];
 
 @NgModule({
--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +8,13 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     console.log('home')
   }
 
   goToUsers() {
-    this.router.navigate(['./user-management']);
+    this.router.navigate(['user-management'], {relativeTo: this.route});
   }
 }
