97009d951c9fe595dbdea85a2eaa2cc2080d4334
unknown <wendyli8620lw@gmail.com>
2022-11-16 20:57:03 +0100

    added some modules and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './main/home/home.component';
+import { UserManagementModule } from './user-management/user-management.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: 'home', component: HomeComponent },
+  { path: 'user-management', loadChildren: () => UserManagementModule }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/user-management/user-management.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserManagementRoutingModule } from './user-management-routing.module';
+import { UsersComponent } from './users/users.component';
+
+
+@NgModule({
+  declarations: [
+    UsersComponent
+  ],
+  imports: [
+    CommonModule,
+    UserManagementRoutingModule
+  ]
+})
+export class UserManagementModule { }
